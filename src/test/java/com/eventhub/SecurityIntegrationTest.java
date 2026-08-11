7a98f9050f0d588ab4552c902ec852b7e403feda
Yash <rajy9234@gmail.com>
2026-08-11 09:04:43 +0530

    test fixed

--- src/test/java/com/eventhub/SecurityIntegrationTest.java
+++ src/test/java/com/eventhub/SecurityIntegrationTest.java
@@ -15,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(properties = {
         "spring.datasource.url=jdbc:h2:mem:eventhub-test;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
         "spring.jpa.hibernate.ddl-auto=create-drop"
 })
 @AutoConfigureMockMvc
